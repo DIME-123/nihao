d075cd4b9374364325af421a062772979d70f0e7
dime <nihao@qq.com>
2025-01-04 20:16:07 +0800

    first commit

---
+++ project/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+import path from 'path'
+export default defineConfig({
+  plugins: [vue()],
+
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,'src')
+    },
+    // 配置代理跨域
+    
+  },
+  // 配置代理跨域
+  server:{
+    proxy:{
+        '/api': {
+        target: 'http://syt.atguigu.cn',
+        changeOrigin: true
+      },
+    }
+  }
+})
